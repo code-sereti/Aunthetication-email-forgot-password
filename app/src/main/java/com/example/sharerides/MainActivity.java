6c3d43d758439032ef0682f928739e6c27d48ab3
Sereti Kelvin <codedroidwithsereti@gmail.com>
2023-10-22 21:27:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sharerides/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sharerides;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView userName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userName = findViewById(R.id.userName);
+
+    }
+}
